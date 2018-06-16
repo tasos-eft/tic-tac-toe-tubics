d173d0e97a8b627e84215b33a691a600f763127f
tasos <tasos@havas.ie>
2018-06-16 22:42:24 +0300

    fix broken rxjs dependancy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppComponent } from './app.component';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { AppRouting } from './app.routing';
 
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+
+import { NodeApiService } from './services/node-api.service';
+import { DataStoreService } from './services/data-store.service';
 
 @NgModule({
   declarations: [
@@ -15,9 +21,12 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    AppRouting,
+    BrowserAnimationsModule,
     CollapseModule.forRoot()
   ],
-  providers: [],
+  providers: [NodeApiService, DataStoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

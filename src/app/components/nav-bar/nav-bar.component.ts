1f827dcdb91e6fbf0041250bfed2712fe4d38fe0
tasos <tasos@havas.ie>
2018-06-17 00:56:03 +0300

    hambuger animation
    jonsuh/hamburgers animation added on hamburger menu

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+  show: boolean;
+  squeeze: boolean;
+  spring: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.show = false;
+    this.squeeze = false;
+    this.spring = false;
+  }
+
+  toggleSpring() {
+    this.show = !this.show;
+    this.spring = !this.spring;
+  }
+
+  toggleSqueeze() {
+    this.squeeze = !this.squeeze;
+    this.show = !this.show;
   }
 
 }
